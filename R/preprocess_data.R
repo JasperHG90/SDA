d91d18bfe92a42eeb48abd7107615c15c402b50a
Jasper Ginn <jasperginn@gmail.com>
2018-11-16 17:35:29 +0100

    Add data preprocess file

---
+++ R/preprocess_data.R
@@ -0,0 +1,14 @@
+## This script pre-processes & joins different data to form final dataset
+## Jasper Ginn
+## SDA project
+## 
+## ----------------------------------------------------------------------------
+##
+## Instructions:
+##  Do not run this file directly. Rather, call it from your main script using
+##
+##     source("practical/preprocess_data.R")
+##
+## ----------------------------------------------------------------------------
+
+library(dplyr)
