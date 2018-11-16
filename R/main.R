0abcbae4f4afa7ab1c5f7c7311f74d110442400c
Jasper Ginn <jasperginn@gmail.com>
2018-11-16 17:35:15 +0100

    Add main file

---
+++ R/main.R
@@ -0,0 +1,17 @@
+## Run this script line-by-line for the main analysis
+## Gabriel, Anne & Jasper
+## Gender paygap project
+## Survey data Analysis
+## Supervisor: Dr. Peter Lugtig
+## Methodology & Statistics Dpt, Utrecht University
+
+# Clean environment
+rm(list=ls())
+
+# Preparation --------
+
+# This script installs packages
+source("R/utilities/install_dependencies.R")
+
+# This script loads and preprocesses the data
+source("R/utilities/load_data.R")
