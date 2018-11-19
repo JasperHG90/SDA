481bf3b84a894aea14b6f1d24b785f319cb7c867
Jasper Ginn <jasperginn@gmail.com>
2018-11-19 08:54:28 +0100

    Basics of preprocessing data

--- R/utilities/preprocess_data.R
+++ R/utilities/preprocess_data.R
@@ -12,3 +12,22 @@
 ## ----------------------------------------------------------------------------
 
 library(dplyr)
+library(tidyr)
+library(stringr)
+
+# Load data
+gpg <- readRDS("data/gender_pay_gap.Rds")
+sic <- readRDS("data/sicCodes.Rds")
+
+# Expand sic codes into multiple columns
+# sic codes are comma-delimited. We split each observation into m separate vectors.
+
+# Find the observation with the maximum number of sic codes
+max_sc <- map_int(gpg$SicCodes, function(x) str_split(x, ",")[[1]] %>%
+                                              length() ) %>%
+  max()
+
+gpg <- gpg %>% # Use the forward-operating pipe to chain together commands
+  separate(SicCodes, c("SC1", "SC2", "SC3"))
+
+# Use regular expressions to retrieve postal code
