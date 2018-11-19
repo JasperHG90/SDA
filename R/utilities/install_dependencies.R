90031685aec82d988c30f5609e8bb0793c9e6337
Jasper Ginn <jasperginn@gmail.com>
2018-11-19 17:20:11 +0100

    Add rvest

--- R/utilities/install_dependencies.R
+++ R/utilities/install_dependencies.R
@@ -15,7 +15,7 @@
 ip <- installed.packages()
 
 # Packages to be installed (just add to the vector if you have new dependencies)
-dependencies <- c("dplyr", "tidyr", "purrr", "stringr")
+dependencies <- c("dplyr", "tidyr", "purrr", "stringr", "rvest")
 
 # For each dependency, install and / or load
 for(dependency in dependencies) {
